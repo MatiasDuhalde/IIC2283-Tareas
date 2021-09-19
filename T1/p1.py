df9bdb66cd33f91daf5e21013aa61df8bd6a4970
Matias Duhalde <matias.duhalde@uc.cl>
2021-09-19 19:10:40 -0300

    :sparkles: Add base

---
+++ T1/p1.py
@@ -0,0 +1,48 @@
+"""
+Solución a problema 1:
+Dado un número N entero positivo, entregar la cantidad de caminos que pasan
+por N aristas, en el multigrafo que representa al problema de los siete puentes
+de Königsberg.
+"""
+import sys
+
+# Grafo
+GRAPH = {
+    0: [1, 1, 2],
+    1: [0, 0, 2, 3, 3],
+    2: [0, 1, 3],
+    3: [1, 1, 2],
+}
+
+# Cache
+CACHE = {
+    0: {0: 1},
+    1: {0: 1},
+    2: {0: 1},
+    3: {0: 1}
+}
+
+
+def get_node_path_number(node, n):
+    cache_res = CACHE[node].get(n)
+    if cache_res:
+        return cache_res
+    else:
+        res = 0
+        for next_node in GRAPH[node]:
+            res += get_node_path_number(next_node, n - 1)
+        CACHE[node][n] = res
+        return res
+
+
+# Obtener N
+n_value = int(input("N: "))
+
+sys.setrecursionlimit(10000)
+
+
+result = 0
+for node in GRAPH:
+    result += get_node_path_number(node, n_value)
+
+print(result % (10 ** 9 + 7))
