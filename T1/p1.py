dd72c4da4b69e2d327593d09da72dacd2aa71bcd
Matias Duhalde <matias.duhalde@uc.cl>
2021-09-19 22:41:16 -0300

    :recycle: Reformulate to matricial operations

--- T1/p1.py
+++ T1/p1.py
@@ -4,47 +4,74 @@ Dado un número N entero positivo, entregar la cantidad de caminos que pasan
 por N aristas, en el multigrafo que representa al problema de los siete puentes
 de Königsberg.
 """
-import sys
+
+# //////////
+# CONSTANTES
+# //////////
 
 # Grafo
-GRAPH = {
-    0: [1, 1, 2],
-    1: [0, 0, 2, 3, 3],
-    2: [0, 1, 3],
-    3: [1, 1, 2],
-}
-
-# Cache
-CACHE = {
-    0: {0: 1},
-    1: {0: 1},
-    2: {0: 1},
-    3: {0: 1}
-}
+GRAPH_MATRIX = [
+    [0, 2, 1, 0],
+    [2, 0, 1, 2],
+    [1, 1, 0, 1],
+    [0, 2, 1, 0],
+]
 
 
-def get_node_path_number(node, n):
-    cache_res = CACHE[node].get(n)
-    if cache_res:
-        return cache_res
-    else:
-        res = 0
-        for next_node in GRAPH[node]:
-            res += get_node_path_number(next_node, n - 1)
-            res %= (10 ** 9 + 7)
-        CACHE[node][n] = res
-        return res
+def GET_IDENTITY_MATRIX():
+    return [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+
+
+MATRIX_DIM = 4
+
+# Módulo (10 ** 9 + 7)
+MOD = 1000000007
+
+# /////////
+# FUNCIONES
+# /////////
+
+
+def sum_matrix(matrix):
+    res = 0
+    for row in matrix:
+        for col in row:
+            res += col
+            res %= MOD
+    return res
+
+
+def matrix_modmult(a, b):
+    res = GET_IDENTITY_MATRIX()
+    for i in range(MATRIX_DIM):
+        for j in range(MATRIX_DIM):
+            for k in range(MATRIX_DIM):
+                res[i][j] += (a[i][k] * b[k][j])
+                res[i][j] %= MOD
+    return res
+
+
+# Source: https://en.wikipedia.org/wiki/Modular_exponentiation#Matrices
+def matrix_modexp(matrix, b):
+    if b == 0:
+        return GET_IDENTITY_MATRIX()
+    if b == 1:
+        return matrix
+    if b % 2 == 1:
+        return matrix_modmult(matrix, matrix_modexp(matrix, b - 1))
+    D = matrix_modexp(matrix, b // 2)
+    return matrix_modmult(D, D)
 
 
 if __name__ == "__main__":
     # Obtener N
     n_value = int(input("N: "))
-
-    sys.setrecursionlimit(10000)
-
-    result = 0
-    for node in GRAPH:
-        result += get_node_path_number(node, n_value)
-        result %= (10 ** 9 + 7)
-
-    print(result)
+    if n_value == 0:
+        print(0)
+    else:
+        print(sum_matrix(matrix_modexp(GRAPH_MATRIX, n_value)) % MOD)
