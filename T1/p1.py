8453c0a5f8df8c0847c0f32d654c3eff7cf75f32
Matias Duhalde <matias.duhalde@uc.cl>
2021-09-19 22:57:52 -0300

    :memo: Add comments

--- T1/p1.py
+++ T1/p1.py
@@ -9,7 +9,7 @@ de Königsberg.
 # CONSTANTES
 # //////////
 
-# Grafo
+# Grafo (representando puentes de Königsberg)
 GRAPH_MATRIX = [
     [0, 2, 1, 0],
     [2, 0, 1, 2],
@@ -19,6 +19,9 @@ GRAPH_MATRIX = [
 
 
 def GET_IDENTITY_MATRIX():
+    """
+    Obtener matriz identidad (dimensiones fijas para este problema)
+    """
     return [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -27,6 +30,7 @@ def GET_IDENTITY_MATRIX():
     ]
 
 
+# Dimensión de la matriz
 MATRIX_DIM = 4
 
 # Módulo (10 ** 9 + 7)
@@ -38,6 +42,11 @@ MOD = 1000000007
 
 
 def sum_matrix(matrix):
+    """
+    Suma todos los elementos de una matriz, calculando el módulo
+    Complejidad fija para dimensión = 4
+    --> O(1)
+    """
     res = 0
     for row in matrix:
         for col in row:
@@ -47,6 +56,11 @@ def sum_matrix(matrix):
 
 
 def matrix_modmult(a, b):
+    """
+    Producto matricial de dos matrices, calculando el módulo
+    Complejidad fija para dimensión = 4
+    --> O(1)
+    """
     res = GET_IDENTITY_MATRIX()
     for i in range(MATRIX_DIM):
         for j in range(MATRIX_DIM):
@@ -58,14 +72,24 @@ def matrix_modmult(a, b):
 
 # Source: https://en.wikipedia.org/wiki/Modular_exponentiation#Matrices
 def matrix_modexp(matrix, b):
+    """
+    Exponenciación modular de una matriz
+    Complejidad dependiente de b
+    """
     if b == 0:
+        # Caso base, constante
         return GET_IDENTITY_MATRIX()
     if b == 1:
+        # Caso base, constante
         return matrix
     if b % 2 == 1:
+        # Producto matricial es constante para dimensiones fijas -> O(1)
+        # Recursión de matrix_modexp, + b
         return matrix_modmult(matrix, matrix_modexp(matrix, b - 1))
-    D = matrix_modexp(matrix, b // 2)
-    return matrix_modmult(D, D)
+    # Recursión T(b/2) -> O(log2(b))
+    res = matrix_modexp(matrix, b // 2)
+    # Producto matricial es constante para dimensiones fijas -> O(1)
+    return matrix_modmult(res, res)
 
 
 if __name__ == "__main__":
@@ -74,4 +98,5 @@ if __name__ == "__main__":
     if n_value == 0:
         print(0)
     else:
+        # Intución: https://stackoverflow.com/a/14272475
         print(sum_matrix(matrix_modexp(GRAPH_MATRIX, n_value)) % MOD)
