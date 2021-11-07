3b8ebbe272e73126cc0b05dcd4609aa60330771d
Matias Duhalde <matias.duhalde@uc.cl>
2021-11-07 05:18:17 -0300

    :sparkles: Add solution to p2.py

---
+++ T2/p2.py
@@ -0,0 +1,62 @@
+"""
+Solucion al problema 2 de la Tarea 2
+"""
+from collections import defaultdict
+from os import read, fstat
+from io import BytesIO
+from random import choices
+from math import ceil, log10
+from copy import copy
+
+EPSILON = 1e-3
+ITERATIONS = 3
+
+
+# Use fast read
+input_buffer = BytesIO(read(0, fstat(0).st_size))
+# Copy read for multiple tests (comment for one test to save resources)
+input_copy = copy(input_buffer)
+input = input_buffer.readline
+
+
+def main():
+    """Main function"""
+    # Read input
+    [n_value, m_value, p_value] = map(int, input().split())
+    binary_values = []
+    for _ in range(n_value):
+        binary_values.append(int(input().strip(), 2))
+
+    # Solve
+    # Iterate times
+    results = defaultdict(int)
+    for _ in range(10**(ITERATIONS)):
+        current = 2**m_value - 1
+        subset = choices(binary_values, k=2*ceil(log10(n_value)))
+        for el in subset:
+            current = current & el
+        results[current] += 1
+    [value, repetitions] = sorted(results.items(), key=lambda x: x[1], reverse=True)[1]
+    binary_result = format(value, f"0{m_value}b")
+    print(binary_result)
+    return binary_result
+
+
+if __name__ == '__main__':
+    # Test once
+    # main()
+
+    # Test several
+    TEST_NUMBER = 100
+    TEST_RESULT = "000000000000000000100100000100101000110000000000000000000000"
+    counter = 0
+    for _ in range(TEST_NUMBER):
+        input_buffer = input_copy
+        input = input_buffer.readline
+        input_copy = copy(input_copy)
+        res = main()
+        if res != TEST_RESULT:
+            counter += 1
+    print("Desired error:", EPSILON)
+    print("Actual error:", counter/TEST_NUMBER)
+    print("Accomplished:", counter/EPSILON <= EPSILON)
