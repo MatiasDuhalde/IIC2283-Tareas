d89c2a243c0b661183ff2989a78444a576cae7b0
Matias Duhalde <matias.duhalde@uc.cl>
2021-12-09 20:04:57 -0300

    :tada: Add base files for T3

---
+++ T3/p1.py
@@ -0,0 +1,17 @@
+"""
+Solucion al problema 1 de la Tarea 3
+"""
+from os import read, fstat
+from io import BytesIO
+
+# Use fast read
+input_buffer = BytesIO(read(0, fstat(0).st_size))
+input = input_buffer.readline  # pylint: disable=redefined-builtin
+
+
+def main():
+    """Main function"""
+
+
+if __name__ == '__main__':
+    main()
